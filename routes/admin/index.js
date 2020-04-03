b15ba9297c3a1115c04fdeecec105f496a0ec674
Oluwatoyosi Oyegoke <oluwatoyosi.oyegoke@gmail.com.com>
2020-04-03 22:48:08 +0100

    add admin route to documentation

---
+++ routes/admin/index.js
@@ -0,0 +1,23 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Admin 
+ *   description: Admin  Routes
+ */
+
+
+ /**
+* @swagger
+* /admin:
+*   get:
+*     summary: Fetches real time data of room bookings, use chrome extension to test.
+*     tags: [Admin]
+*     description:  Fetches real time data of room bookings, use chrome extension to test.
+*     consumes:
+*       — application/json
+*     responses: 
+*       200:
+*         description: Receive a success response.
+*       400:
+*         description: Bad Request.
+*/
